7666387981950f0cf2721e603f78c2f5d6724ce5
Samuel Reichert <samuelreichertt@gmail.com>
2019-10-23 22:02:08 -0300

    feat(*): Setup project with Typescript and styled-components
    * Add Styled Components
    * Add Typescript
    * Add the colors that will be used in the project inside the constants
    file
    * Add the resources folder with how the project will look like
    
    The resources folder has info of how the pages must look like, a readme
    of how it must be done, and a style guide of how to get the font, icons,
    colors similar to the images

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { AppWrapper } from './App.styles'
+
+const App = () => (
+  <AppWrapper>
+    <header>
+      <h1>Countries App</h1>
+    </header>
+  </AppWrapper>
+)
+
+export default App
