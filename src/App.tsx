c6beb371caebe92f5b608dd2e0e159048f533628
Samuel Reichert <samuelreichertt@gmail.com>
2019-11-02 07:54:21 -0300

    feat(*): Add the routes to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
 import React from 'react'
 import Header from './components/Header'
 import CountriesList from './components/CountriesList'
+import CountryDetails from './components/CountryDetails'
 import { AppWrapper } from './App.styles'
-
+import {
+  BrowserRouter as Router, Switch, Route
+} from 'react-router-dom'
 
 const App = () => (
-  <AppWrapper>
-    <Header />
-    {/* Form */}
-    <CountriesList />
-  </AppWrapper>
+  <Router>
+    <AppWrapper>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={CountriesList} />
+        <Route path='/country/:name' component={CountryDetails} />
+      </Switch>
+
+    </AppWrapper>
+  </Router>
 )
 
 export default App
